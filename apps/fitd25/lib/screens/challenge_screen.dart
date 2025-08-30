043a41bf4e40918a5bdc8024904aba432a6d880c
lohnn <johannes@lohnn.se>
2025-08-30 13:31:38 +0200

    Show tooltip of assets

--- apps/fitd25/lib/screens/challenge_screen.dart
+++ apps/fitd25/lib/screens/challenge_screen.dart
@@ -192,21 +192,26 @@ class _ChallengeScreenState extends State<ChallengeScreen>
                             children: [
                               for (final (name, assetContent)
                                   in challenge.assets.records)
-                                ListTile(
-                                  trailing: const Icon(Icons.copy),
-                                  onTap: () {
-                                    Clipboard.setData(
-                                      ClipboardData(text: assetContent),
-                                    );
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(
-                                        content: Text(
-                                          'Copied asset "$name" to clipboard',
+                                Tooltip(
+                                  message: assetContent,
+                                  child: ListTile(
+                                    trailing: const Icon(Icons.copy),
+                                    onTap: () {
+                                      Clipboard.setData(
+                                        ClipboardData(text: assetContent),
+                                      );
+                                      ScaffoldMessenger.of(
+                                        context,
+                                      ).showSnackBar(
+                                        SnackBar(
+                                          content: Text(
+                                            'Copied asset "$name" to clipboard',
+                                          ),
                                         ),
-                                      ),
-                                    );
-                                  },
-                                  title: Text(name),
+                                      );
+                                    },
+                                    title: Text(name),
+                                  ),
                                 ),
                             ],
                           ),
